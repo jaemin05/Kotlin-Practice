9ad73c62ef2fa7c55629879b4ec10a7df5666144
jaemin <sonjm3518@dsm.hs.kr>
2022-07-21 16:50:41 +0900

    📑 :: 변수와 자료형

--- src/VariablesAndDataTypes.kt
+++ src/VariablesAndDataTypes.kt
@@ -1,6 +1,12 @@
 package src
 
-class VariablesAndDataTypes {
-    fun main()
-    }
+fun main() {
+    var a: Int? = null //자료형 뒤에 ?를 붙이면 null을 허용하는 변수로 선언할 수 있다.
+    println(a) //;이 필요없다.
+
+    var b: Long = 123L;
+
+    //char: 2바이트 내부적으로 문자열을 UTF-16 BE로 처리한다.
+    var charValue = 'c'
+    var stringValue = "String"
 }
