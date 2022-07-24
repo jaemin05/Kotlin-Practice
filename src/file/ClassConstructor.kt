f6230027f6c6b8c846ebee5a83721844a8fc5a6f
jaemin <sonjm3518@dsm.hs.kr>
2022-07-25 01:22:09 +0900

    ♻️ :: file 패키지로 이동

---
+++ src/file/ClassConstructor.kt
@@ -0,0 +1,8 @@
+package src
+
+import src.`class`.NewPerson
+
+fun main() {
+    var a = NewPerson("son", 18);
+    var b = NewPerson("kim")
+}
---
+++ src/file/OverridingAndAbstraction.kt
@@ -0,0 +1,12 @@
+package src
+
+import src.`class`.Son
+import src.`class`.UserLogin
+
+fun main() {
+    val s = Son()
+    s.eat()
+
+    var user = UserLogin()
+    user.login()
+}
