432097d4bbcfdacc1fc30931b6d5e06ece5bfbba
jaemin <sonjm3518@dsm.hs.kr>
2022-07-25 01:08:19 +0900

    📑 :: 오브젝트

---
+++ src/Object.kt
@@ -0,0 +1,33 @@
+package src
+
+import src.`class`.FoodPoll
+
+fun main() {
+    println(Counter.count)
+
+    var a = FoodPoll("짜장면")
+    var b = FoodPoll("짬뽕")
+
+    a.vote()
+    a.vote()
+
+    b.vote()
+
+    println("${FoodPoll.total}")
+}
+
+object Counter {
+    var count = 0
+
+    fun countUp() {
+        count += 1
+    }
+
+    fun countDown() {
+        count -= 1
+    }
+
+    fun clear() {
+        count = 0
+    }
+}
