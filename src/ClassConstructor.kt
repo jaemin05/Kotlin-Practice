03379352272d4f2eccef4590f801930e6c2c24bb
jaemin <sonjm3518@dsm.hs.kr>
2022-07-24 23:41:00 +0900

    📑 :: 클래스의 생성자

--- src/ClassConstructor.kt
+++ src/ClassConstructor.kt
@@ -1,2 +1,16 @@
 package src
 
+fun main() {
+    var a = NewPerson("son", 18);
+    var b = NewPerson("kim")
+}
+
+class NewPerson(var name: String, var age: Int) {
+    init {
+        println("저는 ${name}이고 ${age}살 입니다.")
+    }
+
+    constructor(name: String): this(name, 20) {
+        println("-보조 생성자 호출-")
+    }
+}
